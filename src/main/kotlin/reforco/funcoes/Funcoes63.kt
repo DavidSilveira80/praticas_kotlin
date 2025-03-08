81ab78e9ef7399c7bc735351a8512bfb54ea5da8
DavidSilveira80 <davidpscontato@gmail.com>
2025-03-08 16:12:05 -0300

    Funcoes63

---
+++ src/main/kotlin/reforco/funcoes/Funcoes63.kt
@@ -0,0 +1,22 @@
+package reforco.funcoes
+
+/*
+Crie uma função chamada somar que recebe dois números inteiros como parâmetros e imprime a soma deles.
+
+Passo a passo:
+
+Declare uma função chamada somar.
+Adicione dois parâmetros: numero1 e numero2, ambos do tipo Int.
+No corpo da função, calcule a soma dos dois números.
+Use println para exibir o resultado da soma.
+Chame a função com diferentes valores para os parâmetros e verifique o resultado.
+ */
+
+fun somar(numero1 : Int, numero2 : Int){
+    println(numero1 + numero2)
+}
+
+fun main(){
+    somar(2, 2)
+
+}
