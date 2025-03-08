cabe52c0f3158f23ae3c13ac8800fd34ff877a5f
DavidSilveira80 <davidpscontato@gmail.com>
2025-03-08 12:47:32 -0300

    Novo pacote funcoes com exercício Funcoes60.

---
+++ src/main/kotlin/reforco/funcoes/Funcoes60.kt
@@ -0,0 +1,22 @@
+package reforco.funcoes
+
+/*
+Crie uma função chamada saudacao que imprime a mensagem "Olá, mundo!" no console.
+
+Passo a passo:
+
+Declare uma função usando a palavra-chave fun.
+Nomeie a função como saudacao.
+Certifique-se de que a função não receba nenhum parâmetro.
+Dentro do corpo da função, use a função println para exibir a mensagem "Olá, mundo!".
+Chame a função para verificar se ela funciona corretamente.
+ */
+
+
+fun saudacao(){
+    println("Olá, Mundo!")
+}
+
+fun main(){
+    saudacao()
+}
